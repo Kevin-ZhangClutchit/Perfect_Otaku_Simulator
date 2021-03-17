1f38b4248c6de2d2d0dc29f650183745d07efe9b
Kaiwen Zhang <kevin.zhang@sjtu.edu.cn>
2021-03-17 15:15:41 +0800

    commit too low proficiency and I have switched to python

--- simulator.cpp
+++ simulator.cpp
@@ -4,10 +4,19 @@
 
 #include "simulator.h"
 #include <iostream>
-using namespace std;
-int get_param(const string& prompt){
+
+#include <vector>
+int Get_param(const string& prompt){
     int result=1;
     cout<<prompt;
     cin>>result;
     return result;
+}
+bool is_get(int* arr,int target,int arr_size){
+    for (int i = 0; i < arr_size; ++i) {
+        if (*(arr+i)==target){
+            return true;
+        }
+    }
+    return false;
 }
--- simulator.h
+++ simulator.h
@@ -5,7 +5,9 @@
 #ifndef PERFECT_OTAKU_SIMULATOR_SIMULATOR_H
 #define PERFECT_OTAKU_SIMULATOR_SIMULATOR_H
 #include <string>
+#include <vector>
 using namespace std;
 
-int get_param(string prompt);// initialize the setup
+int Get_param(const string& prompt);// initialize the setup
+bool is_get(int* arr,int target,int arr_size);
 #endif //PERFECT_OTAKU_SIMULATOR_SIMULATOR_H
